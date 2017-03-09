415039e176118e43472f2f5474c3423becedf4b1
xd1llenb <xdillenb@gmail.com>
2017-03-09 14:22:36 -0500

    added comments

--- designpatterns/src/main/java/abstractfactory/AbstractFactory.java
+++ designpatterns/src/main/java/abstractfactory/AbstractFactory.java
@@ -1,5 +1,7 @@
 package abstractfactory;
 public abstract class AbstractFactory{
+	// this is a comment for git change
+	// a second line
 	abstract AbstractProductA createProductA();
 	abstract AbstractProductB createProductB();
 }
