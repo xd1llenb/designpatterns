695c90442a4e14aba63f35b3d163fc127d623445
xd1llenb <xdillenb@gmail.com>
2017-03-10 10:45:19 -0500

    branch conflict checkin

--- designpatterns/src/main/java/abstractfactory/AbstractFactory.java
+++ designpatterns/src/main/java/abstractfactory/AbstractFactory.java
@@ -2,7 +2,7 @@ package abstractfactory;
 public abstract class AbstractFactory{
 	// this is a comment for git change
 	// a second line
-	// third comment
+	// conflicting comment
 	abstract AbstractProductA createProductA();
 	abstract AbstractProductB createProductB();
 }
