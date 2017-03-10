2fc15205f103c82f03eb3e25c07a9d9114dfe26d
xd1llenb <xdillenb@gmail.com>
2017-03-10 10:48:43 -0500

    conde conflict from resolve branch

--- designpatterns/src/main/java/abstractfactory/AbstractFactory.java
+++ designpatterns/src/main/java/abstractfactory/AbstractFactory.java
@@ -2,7 +2,7 @@ package abstractfactory;
 public abstract class AbstractFactory{
 	// this is a comment for git change
 	// a second line
-	// third comment
+	// this line on the resolve branch breaks the program
 	abstract AbstractProductA createProductA();
 	abstract AbstractProductB createProductB();
 }
