6f695a8257e3199568c30fa918b3347da5093f8c
xd1llenb <xdillenb@gmail.com>
2017-03-10 14:04:36 -0500

    restore stashed file

--- designpatterns/src/main/java/abstractfactory/AbstractFactory.java
+++ designpatterns/src/main/java/abstractfactory/AbstractFactory.java
@@ -3,6 +3,7 @@ public abstract class AbstractFactory{
 	// this is a comment for git change
 	// a second line
 	// third comment
+	// Test Staching
 	abstract AbstractProductA createProductA();
 	abstract AbstractProductB createProductB();
 }
